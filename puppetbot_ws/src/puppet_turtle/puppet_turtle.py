9f4890a84b4a7a1a9945477974f524fdf0d12e7b
Charles Luo <chunhengluo@outlook.com>
2016-10-15 15:09:11 -0500

    Turtle motion controller completed and tested.

--- puppetbot_ws/src/puppet_turtle/puppet_turtle.py
+++ puppetbot_ws/src/puppet_turtle/puppet_turtle.py
@@ -7,11 +7,26 @@ from turtlesim.msg import Color
 from turtlesim.msg import Pose
 from hlpr_speech_msgs.msg import StampedString
 
+######################################################################
+# ------------------------------------------------------------------ #
+# Puppet Turtle Motion Control Node 
+# 
+#                       ---------------
+#  Speech commands ---> | Motion Ctrl | ---> Turtle motion signals
+#                       ---------------
+# 
+# Author         Chunheng Luo
+# ------------------------------------------------------------------ #
+######################################################################
+
 # Constants
 PI        = 3.1415927
-WALK_SPD  = 0.8
-RUSH_SPD  = 1.0
-TWST_SPD  = 1.0
+WALK_SPD  = 0.7          # Moving speed in WALK state
+RUSH_SPD  = 1.2          # Moving speed in RUSH state 
+TWST_SPD  = 1.0          # Turning speed 
+
+# Node initialization
+rospy.init_node('puppet_turtle')
 
 # Global state variables 
 ######################################################################
@@ -32,31 +47,36 @@ NxtState = 'IDLE'
 # Global control variables
 TunedSpd = 0.8              # Tuned speed applied in state ROAM
 ExitTime = rospy.Time.now() # LEFT/RGHT/ARND timer 
+SpeedInc = 0                # Speed change on SPEED_UP/SLOW_DOWN command
 
 # Speech command subscriber call-back function 
 def StateTrans (msg) : 
   global CurState
   global NxtState
+  global SpeedInc
+  global SpeedDec
 
   if   (msg.keyphrase == 'WALK AHEAD'  ) : 
-    if (CurState == 'IDLE') : 
+    if (CurState != 'WALK') : 
       NxtState = 'WALK' 
     else : 
       NxtState = CurState
 
   elif (msg.keyphrase == 'RUSH AHEAD'  ) : 
-    if (CurState == 'IDLE') : 
+    if (CurState != 'RUSH') : 
       NxtState = 'RUSH' 
     else : 
       NxtState = CurState
 
   elif (msg.keyphrase == 'SPEED UP'    ) : 
+    SpeedInc = 0.1
     if (CurState == 'WALK' or CurState == 'RUSH') : 
       NxtState = 'ROAM' 
     else : 
       NxtState = CurState
 
   elif (msg.keyphrase == 'SLOW DOWN'   ) : 
+    SpeedInc = -0.1
     if (CurState == 'WALK' or CurState == 'RUSH') : 
       NxtState = 'ROAM' 
     else : 
@@ -99,28 +119,34 @@ cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 # Speech command subscriber 
 hlpr_speech_sub = rospy.Subscriber('hlpr_speech_commands', StampedString, StateTrans)
 
-# Node initialization
-rospy.init_node('puppet_turtle')
-
 # State change rate
 rate = rospy.Rate(10)
 
 while not rospy.is_shutdown():
   # State transition 
+  PreState = CurState
   CurState = NxtState 
 
   # LEFT/RGHT/ARND state transition  
-  if   (CurState == 'LEFT' or CurState == 'RGHT') : 
+  if   ((CurState == 'LEFT' and PreState != 'LEFT') or (CurState == 'RGHT' and PreState != 'RGHT')) : 
     ExitTime = rospy.Time.now() + rospy.Duration(PI/2/TWST_SPD)
-  elif (CurState == 'ARND') : 
+  elif (CurState == 'ARND' and PreState != 'ARND') : 
     ExitTime = rospy.Time.now() + rospy.Duration(PI/TWST_SPD)
-  else : 
-    ExitTime = rospy.Time.now()
 
   if ((CurState == 'LEFT' or CurState == 'RGHT' or CurState == 'ARND') and rospy.Time.now() > ExitTime) : 
     CurState = 'IDLE'
+    NxtState = 'IDLE'
 
-  # TODO Speed tuning for ROAM
+  # Speed tuning for ROAM
+  if (CurState == 'ROAM') : 
+    if   (PreState == 'WALK') : 
+      TunedSpd = WALK_SPD + SpeedInc 
+    elif (PreState == 'RUSH') : 
+      TunedSpd = RUSH_SPD + SpeedInc 
+    elif (PreState == 'ROAM') : 
+      # Set low/high speed limit 
+      if (not ((TunedSpd > 2 and SpeedInc > 0) or (TunedSpd < 0.5 and SpeedInc < 0))) : 
+        TunedSpd = TunedSpd + SpeedInc
 
   # Create a new Twist 
   twist = Twist()
@@ -168,98 +194,3 @@ while not rospy.is_shutdown():
   # Wait for the next clock cycle 
   rate.sleep()
 
-# # State transition between moving forward and turning around
-# if driving_forward :
-#   # If there is some obstacles closly ahead, or it has been moving 
-#   # forward for a relatively long time, then stop moving forward
-#   print "[Merry Dol!] Minimum distance: " + str(minDist)
-
-#   collideBorder = nearBorder(myPose) 
-#   collideOthers = minDist < DIST_THRS 
-#   timeExceedLmt = rospy.Time.now() > state_change_time
-
-#   if ( collideBorder or collideOthers or timeExceedLmt ):
-
-#     if ( force_fwd_border and collideBorder and not (collideOthers or timeExceedLmt) ) :
-#       force_fwd_border = False
-#       state_time_ovrid = True
-#       state_change_time = rospy.Time.now() + rospy.Duration(FWD_TIME/20)
-#     elif ( force_fwd_collis and collideOthers and not (collideBorder or timeExceedLmt) ) : 
-#       force_fwd_collis = False 
-#       state_time_ovrid = True
-#       state_change_time = rospy.Time.now() + rospy.Duration(FWD_TIME/10)
-#     else : 
-#       force_fwd_border = False
-#       force_fwd_collis = False 
-#       state_time_ovrid = False
-#       # Stop moving forward and start turning around
-#       driving_forward = False
-#       # Change the direction of twisting
-#       twistFlag = 1 - twistFlag
-
-#     if (collideOthers) :
-#       # mutualAngl = rmTwoPI(myPose.theta - minAngl)
-#       # if (math.fabs(mutualAngl) < 3*PI/4) :
-#       #   if mutualAngl > 0 : 
-#       #     spin_angl = rmTwoPI(3*PI/4 - mutualAngl)
-#       #     twistFlag = 1
-#       #   else :
-#       #     spin_angl = rmTwoPI(3*PI/4 + mutualAngl)
-#       #     twistFlag = 0
-#       # else: 
-#       #   spin_angl = 0 
-#       # Simply turn around 
-#       spin_angl = PI
-
-#       if ( not state_time_ovrid ) :
-#         state_change_time = rospy.Time.now() + rospy.Duration(math.fabs(spin_angl)/TWST_SPD)
-
-#       # Set the force_fwd flag so that the next state is forced to be 
-#       # moving forward. Otherwise the turtle would spin back and force
-#       force_fwd_collis = True
-
-#     elif (collideBorder) :  
-#       # Reflect on border
-#       if (collideBorder == 1): # Left
-#         # Check if the turtle is heading toward the border 
-#         # If so, reflect
-#         # Otherwise, keep going 
-#         if (math.fabs(rmTwoPI(myPose.theta)) > PI/2):
-#           spin_angl = rmTwoPI(PI - myPose.theta - myPose.theta) 
-#         else: 
-#           spin_angl = 0
-#       if (collideBorder == 2): # Down
-#         if (rmTwoPI(myPose.theta) < 0):
-#           spin_angl = rmTwoPI(0 - myPose.theta - myPose.theta) 
-#         else: 
-#           spin_angl = 0
-#       if (collideBorder == 3): # Right
-#         if (math.fabs(rmTwoPI(myPose.theta)) < PI/2):
-#           spin_angl = rmTwoPI(PI - myPose.theta - myPose.theta) 
-#         else: 
-#           spin_angl = 0
-#       if (collideBorder == 4): # Up
-#         if (rmTwoPI(myPose.theta) > 0):
-#           spin_angl = rmTwoPI(0 - myPose.theta - myPose.theta) 
-#         else: 
-#           spin_angl = 0
-
-#       if (spin_angl > 0): 
-#         twistFlag = 1
-#       else:
-#         twistFlag = 0 
-
-#       if ( not state_time_ovrid ) :
-#         state_change_time = rospy.Time.now() + rospy.Duration(math.fabs(spin_angl)/TWST_SPD)
-#       force_fwd_border = True
-
-#     else :
-#       if ( not state_time_ovrid ) :
-#         state_change_time = rospy.Time.now() + rospy.Duration(random.random()*PI/2/TWST_SPD)
-
-# else: # not driving_forward
-#   # If it has been turning around for a relatively long time, then stop turning 
-#   if rospy.Time.now() > state_change_time:
-#     # Stop turning around and start moving forward 
-#     driving_forward = True 
-#     state_change_time = rospy.Time.now() + rospy.Duration(FWD_TIME)
---
+++ puppetbot_ws/src/puppet_turtle/rock_roll.bash
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+./puppet_turtle.py \
+  cmd_vel:=/turtle1/cmd_vel \
+  hlpr_speech_commands:=/hlpr_speech_commands
